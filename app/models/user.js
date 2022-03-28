a3e88c8af0f64fee358c6b2d27dc994a84f2df9e
Juan <juan.alesandri.acc@gmail.com>
2022-03-28 12:29:05 -0300

    Creation of migration and user model

---
+++ app/models/user.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'user',
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+        unique: true
+      },
+      name: {
+        allowNull: false,
+        type: DataTypes.STRING
+      },
+      lastName: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        field: 'last_name'
+      },
+      email: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        unique: true
+      },
+      password: {
+        allowNull: false,
+        type: DataTypes.STRING
+      }
+    },
+    {
+      timestamps: true,
+      underscored: true,
+      tableName: 'users'
+    }
+  );
+  return User;
+};
